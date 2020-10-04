99a1224b8c4149f53f2ff8082f4b681fd6078c8b
JFoxx <spartan1307@gmail.com>
2020-10-04 00:33:29 -0400

    Added counter

--- flutter_app_squares/lib/main.dart
+++ flutter_app_squares/lib/main.dart
@@ -98,13 +98,6 @@ class _MyHomePageState extends State<MyHomePage> {
           // horizontal).
           //mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            /*Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),*/
             Column(
               children: <Widget>[
                 Expanded(
@@ -119,6 +112,17 @@ class _MyHomePageState extends State<MyHomePage> {
                   child: Container(
                     color: Colors.green,
                     width: 300,
+                    child: new Column(
+                      children: <Widget>[
+                        Text(
+                          'You have pushed the button this many times:',
+                        ),
+                        Text(
+                          '$_counter',
+                          style: Theme.of(context).textTheme.headline4,
+                        ),
+                      ],
+                    ),
                   ),
                 ),
                 Expanded(
